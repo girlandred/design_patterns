b80f5aa9db8f752e2838066eb46f3e5c8bd0648a
girlandred <marinka.shita@gmail.com>
2021-12-06 14:43:25 +0200

    Lab 6, second part

---
+++ lab_6/bridge.py
@@ -0,0 +1,39 @@
+class Switch:
+    pass
+
+
+class AutomaticRemoteController(Switch):
+    pass
+
+
+class ManualRemoteController(Switch):
+    pass
+
+
+class EComAppForHome:
+    pass
+
+
+class AC(EComAppForHome):
+    pass
+
+
+class Refrigerator(EComAppForHome):
+    pass
+
+
+class Fan(EComAppForHome):
+    pass
+
+
+class TV(EComAppForHome):
+    pass
+
+
+class GateOpener(EComAppForHome):
+    pass
+
+def main():
+
+if __name__ == "__main__":
+    main()
